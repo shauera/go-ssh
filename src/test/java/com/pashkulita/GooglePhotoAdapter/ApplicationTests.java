8e97e2ffafe09d7b13663ddcdfe3068ac582ba05
Avishalom Shauer <shauera@gmail.com>
2019-12-22 22:18:23 -0500

    Initial commit

---
+++ src/test/java/com/pashkulita/GooglePhotoAdapter/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pashkulita.GooglePhotoAdapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
